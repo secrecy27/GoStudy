33c95b2d06a62d87d453f19e7ba2b32949757051
secrecy27 <secrecy418@naver.com>
2020-05-07 00:27:23 +0900

    검색 크롤링

---
+++ crawling/index.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/anaskhan96/soup"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"os"
+)
+
+type Thumb struct {
+	Thumb []string `json:"thumb" xml:"thumb"`
+	Title []string `json:"title" xml:"title"`
+}
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	//e.Use(middleware.Logger())
+	//e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+	e.GET("/news", news)
+	e.GET("/keywords/:keyword", keywords)
+
+	// Start server
+	e.Start(":1323")
+	//e.Logger.Fatal(e.Start(":1323"))
+}
+
+func keywords(c echo.Context) error {
+	name := c.Param("keyword")
+	fmt.Println("name", name)
+	resp, err := soup.Get("https://search.naver.com/search.naver?query=" + name)
+	if err != nil {
+		os.Exit(1)
+	}
+	doc := soup.HTMLParse(resp)
+	blogThumbs := doc.FindAll("a", "class", "thmb80")
+	fmt.Println(blogThumbs)
+
+	u:=Thumb{
+		//Thumb: "Jon",
+		//Title: "jon@labstack.com",
+	}
+
+	for index, thumb := range blogThumbs {
+		fmt.Println(index, thumb.Text(), " link : ", thumb.Attrs()["href"])
+		//u.Thumb[index]=thumb.Text();
+		//u.Title[index]=thumb.Attrs()["href"]
+	}
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
+	c.Response().WriteHeader(http.StatusOK)
+	return json.NewEncoder(c.Response()).Encode(u)
+}
+
+func news(c echo.Context) error {
+	resp, err := soup.Get("https://www.naver.com")
+	if err != nil {
+		os.Exit(1)
+	}
+	doc := soup.HTMLParse(resp)
+	links := doc.FindAll("a", "class", "nav")
+
+	for _, link := range links {
+		fmt.Println(link.Text(), " link : ", link.Attrs()["href"])
+	}
+	return c.String(http.StatusOK, "links")
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
